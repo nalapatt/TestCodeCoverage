62279055479c20cf2014253590591c56abae47da
Ubuntu <ubuntu@ip-172-31-63-170.ec2.internal>
2020-12-10 15:22:23 +0000

    added new files

---
+++ src/test/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -0,0 +1,30 @@
+/*package com.simplilearn.ReviewAnalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
+
+package com.simplilearn.ReviewAnalyser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyserApplicationTests {
+
+    private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
+    @Test
+    public void testWordCount() {
+   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));
+    }
+}
+
